95aa03e8ea51a3da244b7422d2b8164db614af6c
lucas97s <67008883+lucas97s@users.noreply.github.com>
2025-05-22 19:48:07 +0800

    Finaliz the game play of hangman

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative "lib/game"
 require_relative "lib/storage"
 require "json"
 
-TOTAL_TRIES = 10
+TOTAL_TRIES = 20
 ABSOLUTE_PATH_DICTIONARY = "/Users/vikneswaransathasivam/Documents/repos/hangman/lib/dictionary.txt"
 ABSOLUTE_PATH_SAVED_DATA = "/Users/vikneswaransathasivam/Documents/repos/hangman/lib/saved_data.json"
 
